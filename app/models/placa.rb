6126f430be60f8a26917b51ede0712b7e3aed9b6
Elisangela <elisangela00silva@gmail.com>
2021-02-08 23:05:23 -0300

    modificação na mensagem de validação

--- app/models/placa.rb
+++ app/models/placa.rb
@@ -3,6 +3,6 @@ class Placa < ApplicationRecord
 
 
   validates :placa, :proprietario, :modelo, :ano, presence: { message: "Deve ser preenchido"}
-  validates :placa, format: { with: /[a-zA-Z]{3}[0-9]{4}/, message: "Placa inválida, formato certo: aaa0000" }
-  validates :ano, length: { is: 4 }
+  validates :placa, format: { with: /[a-zA-Z]{3}[0-9]{4}/, message: "Placa inválida (aaa0000)" }
+  validates :ano, length: { is: 4, message: "Apenas 4 números (yyyy)" }
 end

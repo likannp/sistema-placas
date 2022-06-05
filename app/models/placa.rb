cd806ef40df5280d4325d1b15ad57e7b781af7e3
Elis <elisangela00silva@gmail.com>
2022-06-04 22:25:06 -0300

    Update placa.rb

--- app/models/placa.rb
+++ app/models/placa.rb
@@ -3,6 +3,6 @@ class Placa < ApplicationRecord
 
 
   validates :placa, :proprietario, :modelo, :ano, presence: { message: "Deve ser preenchido"}
-  validates :placa, format: { with: /[a-zA-Z]{3}[0-9]{4}/, message: "Placa inválida (aaa0000)" }
+  validates :placa, format: { with: /^[a-zA-Z]{3}[0-9]{4}$/, message: "Placa inválida (aaa0000)" }
   validates :ano, length: { is: 4, message: "Apenas 4 números (yyyy)" }
 end
